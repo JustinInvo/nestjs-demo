f2e79589941c2399e28bab5bc80b50ddd24e02d1
JustinInvo <jusrtin2010@gmail.com>
2024-10-09 00:36:24 -0500

    avance de api

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,5 @@
+// tasks.controller esta mas relacionado trabajar con el cliente
+import { Controller } from '@nestjs/common';
+
+@Controller('tasks')
+export class TasksController {}
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,33 @@
+// tasks.service interactua mas con la base de datos - encargado de interactuar con el schema
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Task } from '../schemas/task.schema';
+import { Model } from 'mongoose';
+import { CreateTaskDto } from '../dto/create-task.dto';
+import { UpdateTaskDto } from '../dto/update-task.dto';
+
+@Injectable()
+export class TasksService {
+  constructor(@InjectModel(Task.name) private taskModel: Model<Task>) {}
+
+  finAll() {
+    this.taskModel.find();
+  }
+
+  async create(createTask: CreateTaskDto) {
+    const newTask = new this.taskModel(createTask);
+    return newTask.save();
+  }
+
+  async findOne(id: string) {
+    return this.taskModel.findById(id);
+  }
+
+  async delete(id: string) {
+    return this.taskModel.findByIdAndDelete(id);
+  }
+
+  async update(id: string, task: UpdateTaskDto) {
+    return this.taskModel.findByIdAndUpdate(id, task);
+  }
+}
